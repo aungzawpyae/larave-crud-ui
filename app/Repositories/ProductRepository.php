efca048c70fc27d5387c2a4ae265017c59de2143
aungzawpyae <aungzawpyaeinf1001mm@gmail.com>
2022-01-10 19:04:28 +0630

    midifefie

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 
 class ProductRepository
 {
-	public function productList(Request $request)
+	public function product(Request $request)
     {
-    	$url = 'api/products';
+    	$url = 'api/product';
     	$params = $request->all();
-
+        
         return fetch_api()->puller($url, $params);
     }
+   
 }
